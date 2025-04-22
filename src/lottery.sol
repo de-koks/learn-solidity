85bc52ecf153f4436419153e53f2583d8522901d
de-koks <denis.kosun@gmail.com>
2025-04-22 12:28:21 +0400

    fix typos and comments

--- src/lottery.sol
+++ src/lottery.sol
@@ -12,11 +12,11 @@ contract Lottery {
         uint256 amountSent;
     }
 
+    Participant[] public participants;
+
     uint public lotteryRoundsTotal;
     uint public participantsTotal;
     uint public prizeSentTotal;
-
-    Participant[] public participants;
 
     constructor() {
         lotteryOrganizer = msg.sender;
@@ -32,17 +32,17 @@ contract Lottery {
             prizeFund += msg.value;
 
             //and check whether the sender has already been added to participants[]
-            bool isParticipantExisrts = false;
+            bool isParticipantExists = false;
             for (uint256 i = 0; i < participants.length; i++) {
                 if(participants[i].participantAddress == msg.sender){
                     //if yes - add msg.value to participant's amountSent
                     participants[i].amountSent += msg.value;
-                    isParticipantExisrts = true;
+                    isParticipantExists = true;
                 }
             }
 
             //if no - add a new participant
-            if (!isParticipantExisrts) {
+            if (!isParticipantExists) {
                 Participant memory newParticipant = Participant(payable (msg.sender), msg.value); // create a new participant with the current value of msg.value and the sender address as its participantAddress
                 participants.push(newParticipant);// push it to the participants[] array so that we can keep track of who has entered
             }
@@ -53,6 +53,9 @@ contract Lottery {
         require(msg.sender == lotteryOrganizer, "Only the lottery organizer can launch the lottery.");
         require(participants.length > 2, "Lottery must have at least 3 participants");
 
+        //create a new array by sorting participants[] by amountSent > 0.5ETH
+        
+        
         //pick a random one among participants[]
         uint256 indexOfRandomParticipant = uint256(keccak256(abi.encodePacked(block.timestamp, block.difficulty))) % participants.length;
         Participant storage winner = participants[indexOfRandomParticipant];  //select a random participant from the participants[] array
@@ -63,7 +66,7 @@ contract Lottery {
         lotteryOrganizerBalance += lotteryOrganizerFee;
 
         //send prize to the winner
-        winner.participantAddress.transfer(prizeFund);// transfer the current value of prizeFund to the address stored in lotteryOrganizer's account
+        winner.participantAddress.transfer(prizeFund);
 
         //record the total logs
         lotteryRoundsTotal++; 
@@ -75,9 +78,10 @@ contract Lottery {
         //now we can conduct another issue of the lottery
     }
 
+    //let the organizer get their reward
     function withdrawLotteryOrganizerFee(uint amount, address payable destination) external {
         require(msg.sender == lotteryOrganizer, "Only the lottery organizer can withdraw.");
-        require(lotteryOrganizerBalance >= amount, "Insufficient lottery orginizer fee fund");
+        require(lotteryOrganizerBalance >= amount, "Insufficient lottery organizer balance");
 
         //send lotteryOrganizerFee to the specified destination
         destination.transfer(amount);
