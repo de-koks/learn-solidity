53f9c22136c4ecc47fa497eace59d01a24c0d73a
Denis Kosun <denis_kosun@epam.com>
2025-05-07 20:20:20 +0400

    adding checks for initial state and negative checks for enterLottery()

--- tests/lottery.spec.ts
+++ tests/lottery.spec.ts
@@ -5,6 +5,9 @@ import path from 'path';
 // Load environment variables from the .env file
 dotenv.config({ path: path.resolve(__dirname, '.env') });
 
+const updatedAmount = 50;
+const updatedNumberOfPlayers = 2;
+
 test.beforeAll(async ({ request }) => {
     // Call the method initializing a new lottery
     const response = await request.post('/transactions', {
@@ -35,9 +38,7 @@ test('lottery should be in the initial state', async ({ request}) => {
                 "chaincode": process.env.LOTTERY
             },
             "func": "getIsInitialState",
-            "args": [
-                
-            ],
+            "args": [],
             "strongread": true
         }
     });
@@ -53,4 +54,140 @@ test('lottery should be in the initial state', async ({ request}) => {
             isInitialState: true
         })
     }));
+});
+
+test('check the initial minimal amount ot enter', async ({ request}) => {
+    const minimalAmountToEnter = await request.post('/query', {
+        data: {
+            "headers": {
+                "signer": process.env.OWNER,
+                "channel": process.env.DEFAULT_CHANNEL,
+                "chaincode": process.env.LOTTERY
+            },
+            "func": "getMinimalAmountToEnter",
+            "args": [],
+            "strongread": true
+        }
+    });
+
+    expect(minimalAmountToEnter.ok()).toBeTruthy();
+
+    const responseJson = await minimalAmountToEnter.json();
+
+    expect(responseJson).toEqual(expect.objectContaining({
+        result: (() => {
+            if (!process.env.MINIMAL_AMOUNT_TO_ENTER) {
+                throw new Error('MINIMAL_AMOUNT_TO_ENTER is not defined in the environment variables');
+            }
+            return parseInt(process.env.MINIMAL_AMOUNT_TO_ENTER, 10);
+        })()
+    }));
+});
+
+test('check the initial minimal number of players', async ({ request}) => {
+    const getMinimalPlayersNumber = await request.post('/query', {
+        data: {
+            "headers": {
+                "signer": process.env.OWNER,
+                "channel": process.env.DEFAULT_CHANNEL,
+                "chaincode": process.env.LOTTERY
+            },
+            "func": "getMinimalPlayersNumber",
+            "args": [],
+            "strongread": true
+        }
+    });
+
+    expect(getMinimalPlayersNumber.ok()).toBeTruthy();
+
+    const responseJson = await getMinimalPlayersNumber.json();
+
+    expect(responseJson).toEqual(expect.objectContaining({
+        result: (() => {
+            if (!process.env.MINIMAL_NUMBER_OF_PLAYERS) {
+                throw new Error('MINIMAL_NUMBER_OF_PLAYERS is not defined in the environment variables');
+            }
+            return parseInt(process.env.MINIMAL_NUMBER_OF_PLAYERS, 10);
+        })()
+    }));
+});
+
+test('should be no players initially', async ({ request}) => {
+    const getPlayers = await request.post('/query', {
+        data: {
+            "headers": {
+                "signer": process.env.OWNER,
+                "channel": process.env.DEFAULT_CHANNEL,
+                "chaincode": process.env.LOTTERY
+            },
+            "func": "getPlayers",
+            "args": [],
+            "strongread": true
+        }
+    });
+
+    expect(getPlayers.ok()).toBeTruthy();
+
+    const responseJson = await getPlayers.json();
+
+    expect(responseJson).toEqual(expect.objectContaining({
+        result: []
+    }));
+});
+
+test('should not allow the owner to enter the lottery', async ({ request}) => {
+    const rejectOwner = await request.post('/transactions', {
+        data: {
+          "headers": {
+              "type": "SendTransaction",
+              "signer": process.env.OWNER,
+              "channel": process.env.DEFAULT_CHANNEL,
+              "chaincode": process.env.LOTTERY,
+            },
+            "func": "enterLottery",
+            "args": [
+              process.env.MINIMAL_AMOUNT_TO_ENTER
+            ],
+            "init": false
+          }
+      });
+
+    expect(rejectOwner.status()).toBe(500);
+
+    const responseJson = await rejectOwner.json();
+
+    expect(responseJson.error).toEqual(
+        expect.stringContaining('The owner cannot enter the lottery.')
+    );
+});
+
+test('should not add a player with insufficient amount', async ({ request}) => {
+    if (!process.env.MINIMAL_AMOUNT_TO_ENTER) {
+        throw new Error('MINIMAL_AMOUNT_TO_ENTER is not defined in the environment variables');
+    }
+    const insufficienAmount = Number(process.env.MINIMAL_AMOUNT_TO_ENTER) - 1;
+    
+    const rejectPlayer = await request.post('/transactions', {
+        data: {
+          "headers": {
+              "type": "SendTransaction",
+              "signer": process.env.PLAYER1,
+              "channel": process.env.DEFAULT_CHANNEL,
+              "chaincode": process.env.LOTTERY,
+            },
+            "func": "enterLottery",
+            "args": [
+              insufficienAmount.toString()
+            ],
+            "init": false
+          }
+      });
+
+    expect(rejectPlayer.status()).toBe(500);
+
+    const responseJson = await rejectPlayer.json();
+
+    expect(responseJson.error).toEqual(
+        expect.stringContaining(`Entry amount must be at least ${process.env.MINIMAL_AMOUNT_TO_ENTER}`)
+    );
 });
